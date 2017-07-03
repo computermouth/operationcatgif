ca7dc6346e006d78d8cdc1238bb9cfe661037698
computermouth <computermouth@crunchbangplusplus.org>
2017-07-03 12:49:45 -0700

    Fixed up some infrastructure and code errors

--- nodeio/index.js
+++ nodeio/index.js
@@ -10,59 +10,51 @@ const CHIPPRO_GPIO_DIRE = '/sys/class/gpio/gpio132/direction'
 const CHIPPRO_GPIO_VALU = '/sys/class/gpio/gpio132/value'
 const FILE_NOT_FOUND_CODE = 'ENOENT'
 
-
-// check for the gpio directory
+// check if gpio root exists
 fs.readdir(CHIPPRO_GPIO_ROOT, (err, files) => {
   if (err) logAndExit(err.message)
-  
-  // see if the pin needs to be exported
-  fs.readdir(CHIPPRO_GPIO_P_D0, (err, files) => {
-    if (err && err !== FILE_NOT_FOUND_CODE) logAndExit(err.message)
-    if (err === FILE_NOT_FOUND_CODE) {
-      
-      // check if export file exists
-      if (fs.existsSync(CHIPPRO_GPIO_EXPO)) {
-        
-        // export the pin
-        fs.writeFileSync(CHIPPRO_GPIO_EXPO, CHIPPRO_GPIO_P_NO)
-        console.log(`pin ${CHIPPRO_GPIO_P_NO} was exported`)
-      } else {
-        logAndExit(`${CHIPPRO_GPIO_EXPO} does not exist, or couldn't be accessed`)
-      }
-    } else {
-      console.log(`pin ${CHIPPRO_GPIO_EXPO} was found to have been previously exported`)
-    }
-    
-    // check if direction file exists
-    if (fs.existsSync(CHIPPRO_GPIO_DIRE)) {
-      
-      // set direction to 'out'
-      fs.writeFileSync(CHIPPRO_GPIO_DIRE, "out")
-    } else {
-      logAndExit(`${CHIPPRO_GPIO_DIRE} does not exist`)
-    }
-    
-    
-    let value = 1
-    setInterval(() => {
-      
-      // check if value file exists
-      if (fs.existsSync(CHIPPRO_GPIO_VALU)) {
-        fs.writeFileSync(CHIPPRO_GPIO_VALU, value)
-        
-        if (value === 1) {
-          value = 0
-        } else {
-          value = 1
-        }
-        
-        console.log(`${CHIPPRO_GPIO_VALU} was set to ${value}`)
-      } else {
-        logAndExit(`${CHIPPRO_GPIO_VALU} does not exist, or couldn't be accessed`)
-      }
-    }, 1000)
-  })
 })
+
+// check if pin directory exists
+if (fs.existsSync(CHIPPRO_GPIO_P_NO)) {
+  console.log(`pin ${CHIPPRO_GPIO_EXPO} was found to have been previously exported`)
+} else {
+    // pin not exported, check if export file exists
+    if (fs.existsSync(CHIPPRO_GPIO_EXPO)) {
+      // export the pin
+      fs.writeFileSync(CHIPPRO_GPIO_EXPO, CHIPPRO_GPIO_P_NO)
+    } else {
+      logAndExit(`${CHIPPRO_GPIO_EXPO} does not exist`)
+    }
+}
+    
+// check if direction file exists
+if (fs.existsSync(CHIPPRO_GPIO_DIRE)) {
+  // set direction to 'out'
+  fs.writeFileSync(CHIPPRO_GPIO_DIRE, "out")
+} else {
+  logAndExit(`${CHIPPRO_GPIO_DIRE} does not exist`)
+}
+
+
+let value = 1
+setInterval(() => {
+  
+  // check if value file exists
+  if (fs.existsSync(CHIPPRO_GPIO_VALU)) {
+    fs.writeFileSync(CHIPPRO_GPIO_VALU, value)
+    
+    if (value === 1) {
+      value = 0
+    } else {
+      value = 1
+    }
+    
+    console.log(`${CHIPPRO_GPIO_VALU} was set to ${value}`)
+  } else {
+    logAndExit(`${CHIPPRO_GPIO_VALU} does not exist, or couldnt be accessed`)
+  }
+}, 1000)
 
 function logAndExit(message) {
   console.error(message)
